a0871b340cb37de3b139144b7ffd6e6d7779a33a
Aymendhibi <dhibi.aymen@esprit.tn>
2024-03-06 09:23:25 +0100

    table chauffeur

---
+++ src/main/java/com/springjwt/entities/Chauffeur.java
@@ -0,0 +1,37 @@
+package com.springjwt.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Chauffeur {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    private String nom;
+
+
+    private String prenom;
+    private String mail;
+
+
+    private String numTel;
+
+
+
+    @OneToMany(mappedBy = "chauffeur")
+    private List<Commande> commande;
+
+
+}
--- src/main/java/com/springjwt/entities/Commande.java
+++ src/main/java/com/springjwt/entities/Commande.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 import java.util.Set;
@@ -18,6 +19,8 @@ public class Commande {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date dateCommande;
     private Date dateLivraison;
     private String statut;
@@ -26,5 +29,15 @@ public class Commande {
     private Depot depot;
     @OneToMany(cascade = CascadeType.ALL, mappedBy="commande")
     private Set<LigneCommande> LigneCommandes;
-
+    public void calculateTotal() {
+        double total = 0.0;
+        if (LigneCommandes != null) {
+            for (LigneCommande ligneCommande : LigneCommandes) {
+                total += ligneCommande.getQuantite() * ligneCommande.getPrixUnitaire();
+            }
+        }
+        this.montant = total;
+    }
+    @ManyToOne
+    private Chauffeur  chauffeur;
 }
